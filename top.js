08357cb69b274c8bd21237040868134ecdf46fa8
Tabata777 <91420047+Tabata777@users.noreply.github.com>
2021-09-26 22:26:30 +0900

    Create top.js

---
+++ top.js
@@ -0,0 +1,68 @@
+/*==================================================
+共通部分読み込み
+==================================================*/
+$(function() {
+    $("#header").load("common/header-top.html");
+    $("#footer").load("common/footer.html");
+});
+
+/*==================================================
+メインビジュアル_スライドショー
+==================================================*/
+$('.slider').slick({
+    //自動的に動き出すか。初期値はfalse
+    autoplay: true,
+    //スライドをループさせるかどうか。初期値はtrue
+    infinite: true,
+    //スライドを画面に3枚見せる
+    slidesToShow: 1,
+    //1回のスクロールで3枚の写真を移動して見せる
+    slidesToScroll: 1,
+    //矢印部分PreviewのHTMLを変更
+    prevArrow: '<div class="slick-prev"></div>',
+    //矢印部分NextのHTMLを変更
+    nextArrow: '<div class="slick-next"></div>',
+    //下部ドットナビゲーションの表示
+    dots: true,
+});
+
+/*==================================================
+最近チェックした商品（矢印、フリック操作）
+==================================================*/
+$('.check-pic').slick({
+    //自動的に動き出すか。初期値はfalse
+    autoplay: false,
+    //スライドをループさせるかどうか。初期値はtrue
+    infinite: true,
+    //スライドを画面に3枚見せる
+    slidesToShow: 1,
+    //1回のスクロールで3枚の写真を移動して見せる
+    slidesToScroll: 1,
+    //矢印部分PreviewのHTMLを変更
+    prevArrow: '<div class="slick-prev"></div>',
+    //矢印部分NextのHTMLを変更
+    nextArrow: '<div class="slick-next"></div>',
+    //下部ドットナビゲーションの表示
+    dots: true,
+    responsive: [{
+            //モニターの横幅が769px以下の見せ方
+            breakpoint: 769,
+            settings: {
+                //スライドを画面に2枚見せる
+                slidesToShow: 1,
+                //1回のスクロールで2枚の写真を移動して見せる
+                slidesToScroll: 1,
+            }
+        },
+        {
+            //モニターの横幅が426px以下の見せ方
+            breakpoint: 426,
+            settings: {
+                //スライドを画面に1枚見せる
+                slidesToShow: 1,
+                //1回のスクロールで1枚の写真を移動して見せる
+                slidesToScroll: 1,
+            }
+        }
+    ]
+});
